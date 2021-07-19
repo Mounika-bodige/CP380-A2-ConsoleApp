63f219cb99e20a840d90f879b8cb3982c67d1c88
Mounika-bodige <65857924+Mounika-bodige@users.noreply.github.com>
2021-07-19 14:49:34 -0400

    Update Program.cs

--- BreadmakerReport/Program.cs
+++ BreadmakerReport/Program.cs
@@ -1,8 +1,9 @@
-﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Linq;
 using RatingAdjustment.Services;
 using BreadmakerReport.Models;
+using System.Collections.Generic;
 
 namespace BreadmakerReport
 {
@@ -10,23 +11,60 @@ namespace BreadmakerReport
     {
         static string dbfile = @".\data\breadmakers.db";
         static RatingAdjustmentService ratingAdjustmentService = new RatingAdjustmentService();
+        private static int j;
 
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Bread World");
             var BreadmakerDb = new BreadMakerSqliteContext(dbfile);
-            var BMList = BreadmakerDb.Breadmakers
-                // TODO: add LINQ logic ...
-                //       ...
-                .ToList();
+            var BMList = BreadmakerDb.Breadmakers;
+            var allids = BMList.Select(s => s);
+            List<string> Makersid = new List<string>();
+           List<Mydata> Alist = new List<Mydata>();
+
+            foreach (var id in allids)
+            {
+                Makersid.Add(id.BreadmakerId);
+            }
+
+            foreach(var ids in Makersid)
+            {
+                var reviews = BMList.Where(letter => letter.BreadmakerId == ids)
+               .Select(rew => rew.Reviews);
+
+                foreach(var eachrew in reviews)
+                {
+                    double count = 0.0; double numbers = 0.0;string titlehold = "";
+                    for (int i = 0; i < eachrew.Count; i++)
+                    {
+                        count = count + eachrew[i].stars;
+                        numbers++;
+                    }
+                    foreach (var idas in allids)
+                    {
+                        if (ids == idas.BreadmakerId)
+                        {
+                            titlehold = idas.title;
+                        }
+                    }
+                    Mydata val = new Mydata();
+                    val.Reviews=numbers;
+                    val.Average= Math.Round(count/ numbers, 2);
+                    val.Adjust= ratingAdjustmentService.Adjust(Math.Round(count / numbers, 2), numbers);
+                    val.Title = titlehold;
+                    Alist.Add(val);
+                }
+
+            }
+           Alist = Alist
+                  .OrderByDescending(Adjus => Adjus.Adjust)
+                  .ToList();
 
             Console.WriteLine("[#]  Reviews Average  Adjust    Description");
             for (var j = 0; j < 3; j++)
             {
-                var i = BMList[j];
-                // TODO: add output
-                // Console.WriteLine( ... );
-            }
+            Console.WriteLine(j + 1 + "\t" + Alist[j].Reviews + "\t" + Alist[j].Average + "\t" + Alist[j].Adjust + "\t" + Alist[j].Title);
+        }
         }
     }
 }
